4325124681f5e9067641bc9dd298a7b62cd316a4
Dominic Ndondo <59946287+Dominic263@users.noreply.github.com>
2023-09-08 16:14:40 -0400

    Project Setup - importing swift-composable-architecture and setting up the relevant libraries + TabView and Initial views.

---
+++ RideSync/RideSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideSyncApp.swift
+//  RideSync
+//
+//  Created by DOMINIC NDONDO on 9/8/23.
+//
+
+import SwiftUI
+
+@main
+struct RideSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
